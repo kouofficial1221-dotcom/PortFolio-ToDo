2e0357c7c703ce3c8e9d4813ae5d123062c79098
kouofficial1221-dotcom <kou.official1221@gmail.com>
2025-11-20 00:20:26 +0900

    初回コミット

---
+++ Form1.Designer.vb
@@ -0,0 +1,178 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        TabControl1 = New TabControl()
+        TabPage1 = New TabPage()
+        DataGridView1 = New DataGridView()
+        TabPage2 = New TabPage()
+        BindingSource1 = New BindingSource(components)
+        Label1 = New Label()
+        Label2 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        ComboBox1 = New ComboBox()
+        ComboBox2 = New ComboBox()
+        TabControl1.SuspendLayout()
+        TabPage1.SuspendLayout()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(BindingSource1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' TabControl1
+        ' 
+        TabControl1.Controls.Add(TabPage1)
+        TabControl1.Controls.Add(TabPage2)
+        TabControl1.Location = New Point(336, 107)
+        TabControl1.Margin = New Padding(3, 2, 3, 2)
+        TabControl1.Name = "TabControl1"
+        TabControl1.SelectedIndex = 0
+        TabControl1.Size = New Size(440, 430)
+        TabControl1.TabIndex = 0
+        ' 
+        ' TabPage1
+        ' 
+        TabPage1.Controls.Add(DataGridView1)
+        TabPage1.Location = New Point(4, 24)
+        TabPage1.Margin = New Padding(3, 2, 3, 2)
+        TabPage1.Name = "TabPage1"
+        TabPage1.Padding = New Padding(3, 2, 3, 2)
+        TabPage1.Size = New Size(432, 402)
+        TabPage1.TabIndex = 0
+        TabPage1.Text = "TabPage1"
+        TabPage1.UseVisualStyleBackColor = True
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(18, 52)
+        DataGridView1.Margin = New Padding(3, 2, 3, 2)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 51
+        DataGridView1.Size = New Size(262, 141)
+        DataGridView1.TabIndex = 0
+        ' 
+        ' TabPage2
+        ' 
+        TabPage2.Location = New Point(4, 24)
+        TabPage2.Margin = New Padding(3, 2, 3, 2)
+        TabPage2.Name = "TabPage2"
+        TabPage2.Padding = New Padding(3, 2, 3, 2)
+        TabPage2.Size = New Size(432, 402)
+        TabPage2.TabIndex = 1
+        TabPage2.Text = "TabPage2"
+        TabPage2.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.Font = New Font("Yu Gothic UI", 50.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(128))
+        Label1.Location = New Point(60, 61)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(250, 100)
+        Label1.TabIndex = 1
+        Label1.Text = "mm:ss"
+        Label1.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label2
+        ' 
+        Label2.BackColor = Color.FromArgb(CByte(255), CByte(128), CByte(128))
+        Label2.Font = New Font("メイリオ", 12.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(128))
+        Label2.Location = New Point(60, 217)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(250, 50)
+        Label2.TabIndex = 3
+        Label2.Text = "yyyy/mm/dd hh:mm:ss"
+        Label2.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(60, 422)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 4
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(217, 422)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 5
+        Button2.Text = "Button2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"25分(推奨)", "30分"})
+        ComboBox1.Location = New Point(40, 349)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(121, 23)
+        ComboBox1.TabIndex = 6
+        ' 
+        ' ComboBox2
+        ' 
+        ComboBox2.FormattingEnabled = True
+        ComboBox2.Items.AddRange(New Object() {"15", "20(推奨)", "25"})
+        ComboBox2.Location = New Point(200, 349)
+        ComboBox2.Name = "ComboBox2"
+        ComboBox2.Size = New Size(121, 23)
+        ComboBox2.TabIndex = 7
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(784, 561)
+        Controls.Add(ComboBox2)
+        Controls.Add(ComboBox1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(TabControl1)
+        Margin = New Padding(3, 2, 3, 2)
+        Name = "Form1"
+        Text = "Form1"
+        TabControl1.ResumeLayout(False)
+        TabPage1.ResumeLayout(False)
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        CType(BindingSource1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents BindingSource1 As BindingSource
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox2 As ComboBox
+
+End Class
